f65be424bc8231593785674bff59882dc0f8abf7
kylead10 <adiokayode111@gmail.com>
2024-02-23 12:58:18 +0100

    footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
-
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='sticky bottom-0 py-6 bg-zinc-50'>
+      <div className='max-w-7xl mx-auto px-3'>
+        <div className='flex justify-center items-center'>
+          <span className='text-zinc-700 text-sm'>
+            &copy; Gregory Berikidis
+          </span>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
