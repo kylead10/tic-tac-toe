ac0f38b20359fbed50e0bef9974152c60fa5b730
kylead10 <adiokayode111@gmail.com>
2024-03-18 13:25:57 +0100

    footer jsx fix

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,9 +3,7 @@ const Footer = () => {
     <footer className='sticky bottom-0 py-6 bg-zinc-50'>
       <div className='max-w-7xl mx-auto px-3'>
         <div className='flex justify-center items-center'>
-          <span className='text-zinc-700 text-sm'>
-            &copy; Gregory Berikidis
-          </span>
+          <span className='text-zinc-700 text-sm'>&copy; Kayode </span>
         </div>
       </div>
     </footer>
